817dca59f6cbcc90a575a8b7980ffee04f6adabe
Wojciech K <wojtekkk@protonmail.com>
2021-12-14 13:55:35 +0100

    code formatted

--- src/App.js
+++ src/App.js
@@ -17,12 +17,8 @@ function App() {
   return (
     <Container>
       <Header title="Lista zadań" />
-
       <Section title="Dodaj nowe zadanie" body={<Form />} />
-
-      <Section title="Lista zadań" body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
-
+      <Section title="Lista zadań" body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />} extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
     </Container>
   );
 }
