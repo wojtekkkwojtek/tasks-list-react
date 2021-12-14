28c84f35f4d0deb75119d03fafc4b82965642ec5
Wojciech K <wojtekkk@protonmail.com>
2021-12-14 13:53:38 +0100

    key added to a list element

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,10 +1,11 @@
 import "./style.css"
 
-
 const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li className={`tasks__item ${task.done && hideDoneTasks ? "tasks__item--hidden" : ""}`}>
+            <li
+                key={task.id}
+                className={`tasks__item ${task.done && hideDoneTasks ? "tasks__item--hidden" : ""}`}>
                 <button className="tasks__button tasks__button--toggleDone">
                     {task.done ? "✓" : ""}
                 </button>
