ce752bb3bb9e17fb3c5e44838874f502978de157
Wojciech K <wojtekkk@protonmail.com>
2022-01-03 21:16:43 +0100

    code formatted

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -6,11 +6,17 @@ const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button onClick={toggleHideDone} className="buttons__button">
-                    {hideDone ? "Pokaż" : "Ukryj"} zrobione
+                <button 
+                onClick={toggleHideDone} 
+                className="buttons__button">
+                    {hideDone ? "Pokaż" : "Ukryj"} 
+                        zrobione
                 </button>
-                <button onClick={setAllDone} className="buttons__button"
-                    disabled={tasks.every(({ done }) => done)}>
+                
+                <button 
+                onClick={setAllDone} 
+                className="buttons__button"
+                disabled={tasks.every(({ done }) => done)}>
                     Ukończ wszystkie
                 </button>
             </>
