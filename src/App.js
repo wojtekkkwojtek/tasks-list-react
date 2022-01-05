e3c8360364c74c5dcab5dedf464059886f6de766
Wojciech K <wojtekkk@protonmail.com>
2022-01-05 20:24:11 +0100

    code formatted

--- src/App.js
+++ src/App.js
@@ -37,35 +37,50 @@ function App() {
   };
   
   const addNewTask = (content) => {
-    setTasks(tasks => 
-      [...tasks, { content,
-         done: false,
-          id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1 }]);
+    setTasks(tasks => [
+      ...tasks, 
+      { 
+        content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1, 
+      },
+    ]);
   };
   
   return (
     <Container>
-      <Header title="Lista zadań" />
+      <Header title="Lista zadań" 
+      />
       
       <Section 
-      title="Dodaj nowe zadanie" 
-      body={<Form addNewTask={addNewTask} />} />
+        title="Dodaj nowe zadanie" 
+        body={
+          <Form addNewTask={addNewTask} 
+          />
+        } 
+      />
       
       <Section 
-      title="Lista zadań"
-      body={<Tasks 
-        tasks={tasks} 
-        hideDone={hideDone} 
-        removeTask={removeTask} 
-        toggleTaskDone={toggleTaskDone} />}
-      extraHeaderContent={<Buttons 
-        tasks={tasks} 
-        hideDone={hideDone} 
-        toggleHideDone={toggleHideDone} 
-        setAllDone={setAllDone} />} />
-    
-    </Container>
-  );
-}
+        title="Lista zadań"
+        body={
+          <Tasks 
+          tasks={tasks} 
+          hideDone={hideDone} 
+          removeTask={removeTask} 
+          toggleTaskDone={toggleTaskDone} 
+          />
+        }
+        extraHeaderContent={
+          <Buttons 
+            tasks={tasks} 
+            hideDone={hideDone} 
+            toggleHideDone={toggleHideDone} 
+            setAllDone={setAllDone} 
+            />
+          } 
+        />
+      </Container>
+    );
+  }
 
 export default App;
