5a7d0c88c3c8ca3e52e27ad0bb3746a2e87f90fe
Wojciech K <wojtekkk@protonmail.com>
2022-02-02 18:51:54 +0100

    GlobalStyle.js created instead of index.css

---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        background:rgb(178, 199, 218);
+    }
+`;
