aad2bab85558f65ba8f493c7d1340f02513b1e15
Wojciech K <wojtekkk@protonmail.com>
2022-02-02 18:48:46 +0100

    A styled-component created for Container

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,8 @@
-.container {
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
     max-width: 900px;
     padding: 20px;
     margin: 0 auto;
     font-family: 'Roboto', sans-serif;
-}
+`;
