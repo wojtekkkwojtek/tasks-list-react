cbcba596289d0eb5400366e66319c2bd8be83b09
Wojciech K <wojtekkk@protonmail.com>
2022-02-02 18:50:17 +0100

    A styled-component created for Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,16 +1,16 @@
-import "./style.css"
+import {Wrapper, Header, Body, Title} from "./styled"
 
 const Section = ({ title, body, extraHeaderContent }) => (
 
-    <section className="section">
-        <header className="section__header">
-            <h2 className="section__title">{title}</h2>
+    <Wrapper>
+        <Header>
+            <Title>{title}</Title>
             {extraHeaderContent}
-        </header>
-        <div className="section__body">
+        </Header>
+        <Body>
             {body}
-        </div>
-    </section>
+        </Body>
+    </Wrapper>
 
 );
 
--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,30 +1,30 @@
-.section {
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
     margin: 10px 0;
     background: white;
     box-shadow: 0 0 5px #ddd;
-}
+`;
 
-.section__header {
+export const Header = styled.header`
     border-bottom: 1px solid #ddd;
     display: grid;
     grid-template-columns: auto auto;
     grid-gap: 20px;
     justify-content: space-between;
     align-items: center;
-    padding: 20px
-}
+    padding: 20px;
 
-@media (max-width: 767px) {
-    .section__header {
+    @media (max-width: 767px) {
         grid-template-columns: 1fr;
     }
-}
+`;
 
-.section__body {
+export const Body = styled.div`
     padding: 20px;
-}
+`;
 
-.section__title {
+export const Title = styled.h2`
     font-size: 1.5em;
     margin: 0;
-}
+`;
