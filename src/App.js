1456ca3776d53aae0555971ed924c01246bb44bf
Wojciech K <wojtekkk@protonmail.com>
2022-03-10 20:29:58 +0100

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import Tasks from './features/tasks/Tasks';
+import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
+import {toAuthor, toTask, toTasks} from './routes';
+import {AuthorPage} from './features/author/AuthorPage';
+import TasksPage from './features/tasks/TasksPage';
+import TaskPage from './features/tasks/TaskPage';
+import {Navigation} from './Navigation';
 
-const App = () => <Tasks/>;
-
-export default App ;
+export const App = () => (
+    <HashRouter>
+        <Navigation/>
+            <Switch>
+                <Route path={toTask()}>
+                    <TaskPage />
+                </Route>
+                <Route path={toTasks()}>
+                    <TasksPage />
+                </Route>
+                <Route path={toAuthor()}>
+                    <AuthorPage />
+                </Route>
+                <Route>
+                    <Redirect to={toTasks()}/>
+                </Route>    
+            </Switch>
+    </HashRouter>
+);
