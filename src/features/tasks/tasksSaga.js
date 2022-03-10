5d49210f0fc70fb3230056d5e939833219f716c8
Wojciech K <wojtekkk@protonmail.com>
2022-03-10 20:26:42 +0100

    tasksSaga properties modified

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,14 +1,25 @@
-import {delay, takeLatest, takeEvery, call, put, select} from "redux-saga/effects";
+import {delay, 
+    takeLatest, 
+    takeEvery, 
+    call, 
+    put, 
+    select
+} from "redux-saga/effects";
 import {getExampleTasks} from "./getExampleTasks";
-import {saveTasksInLocalStorage} from "../tasksLocalStorage";
-import {fetchExampleTasks, setTasks, selectTasks} from "./tasksSlice"; 
+import {saveTasksInLocalStorage} from "./tasksLocalStorage";
+import {fetchExampleTasks, 
+    fetchExampleTasksSuccess, 
+    fetchExampleTasksError, 
+    selectTasks
+} from "./tasksSlice"; 
 
 function* fetchExampleTasksHandler() {
     try {
-        yield delay(1000);
+        yield delay(3000);
         const exampleTasks = yield call(getExampleTasks); 
-        yield put(setTasks(exampleTasks));
+        yield put(fetchExampleTasksSuccess(exampleTasks));
     } catch(error){
+        yield put(fetchExampleTasksError());
         yield call(alert, "Something went wrong :/");
     }
 }
