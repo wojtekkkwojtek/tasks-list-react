d00bedb082b679623afade357d21e2d26208624d
Wojciech K <wojtekkk@protonmail.com>
2022-03-14 17:45:41 +0100

    value of List max-width changed

--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -25,7 +25,7 @@ export const List = styled.ul`
     display: flex;
     justify-content: center;
     list-style: none;
-    max-width: 800px;
+    max-width: 860px;
 `;
 
 export const Item = styled.li`
